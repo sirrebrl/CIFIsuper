a79257c70db053adab8380adfe3598ab2aaae223
sirrebrl <62961956+sirrebrl@users.noreply.github.com>
2023-05-12 14:06:01 -0600

    Update AcademyPortal.js

--- AcademyPortal.js
+++ AcademyPortal.js
@@ -1,208 +1,154 @@
-// const academyPanel =
-// {
-//     width: 10,
-//     height: 10,
-//     panelCSS: null,
-//     active: ''
-// };
-
-function constructAcademyPanel(width, height, section)
+function CalculateFarmTimes(getRawTime = false)
 {
-    academyPanel.width = width;
-    academyPanel.height = height;
-
-    const panelElem = document.createElement('div');
-    document.body.append(panelElem);
-    panelElem.classList.add('panel');
-    panelElem.style.width = `${academyPanel.width}px`;
-    panelElem.style.height = `${academyPanel.height}px`;
-    academyPanel.elem = panelElem;
-
-    if (section.includes('farms')) { constructFarmPanel(); }
-    else if (section.includes('projects')) { constructProjectPanel(); }
-    else if (section.includes('gear')) { constructGearPanel(); }
-    else if (section.includes('badges')) { constructBadgePanel(); }
-}
-
-function resizeAcademyPanel(width, height)
-{
-    academyPanel.width = width;
-    academyPanel.height = height;
-    academyPanel.elem.style.width = `${academyPanel.width}px`;
-    academyPanel.elem.style.height = `${academyPanel.height}px`;
-
-    if (academyPanel.active === 'farms') { resizeFarmPanel(); }
-    else if (academyPanel.active === 'projects') { resizeProjectPanel(); }
-    else if (academyPanel.active === 'gear') { resizeGearPanel(); }
-    else if (academyPanel.active === 'badges') { resizeBadgePanel(); }
-}
-
-function destroyAcademyPanel()
-{
-    document.body.removeChild(academyPanel.elem);
-    academyPanel.elem = null;
-    document.body.removeChild(academyPanel.panelCSS);
-    academyPanel.panelCSS = null;
-}
-
-function constructFarmPanel()
-{
-    const lineHeight = academyPanel.height / 24;
-
-    const zeusSection =
+    let farmData = [];
+    for (let planet = 0; planet < GameDB.academy.planets; planet++)
     {
-        width: academyPanel.width / 2.1,
-        height: academyPanel.height / 8
-    };
+        for (let farm = 0; farm < 3; farm++)
+        {
+            let power = 0;
+            let population = 0;
+            power += playerData.academy.farms[planet][farm].pods * playerData.academy.personnel[0].power;
+            population += playerData.academy.farms[planet][farm].pods;
+            power += playerData.academy.farms[planet][farm].fireteams * playerData.academy.personnel[1].power;
+            population += playerData.academy.farms[planet][farm].fireteams;
+            power += playerData.academy.farms[planet][farm].titans * playerData.academy.personnel[2].power;
+            population += playerData.academy.farms[planet][farm].titans;
+            power += playerData.academy.farms[planet][farm].corvettes * playerData.academy.personnel[3].power;
+            population += playerData.academy.farms[planet][farm].corvettes;
 
-    const modSection =
-    {
-        width: academyPanel.width / 2.1,
-        height: academyPanel.height / 8
-    };
-    modSection.left = academyPanel.width - modSection.width;
+            power *= (playerData.academy.badges.engineering + 1);
+            power *= Math.pow(1.05, Math.floor(playerData.research.missionAnalysis3 + 1) / 2);
+            power *= Math.pow(1.03, playerData.loopMods.swarm);
+            console.log(power);
 
-    const researchSection =
-    {
-        width: academyPanel.width / 2.1,
-        height: academyPanel.height / 16
-    };
+            if (power === 0)
+            {
+                if (getRawTime)
+                {
+                    farmData.push({time: 1e10, personnel: population}); continue;
+                }
+                farmData.push({time: '', personnel: 0}); continue;
+            }
 
-    const academySection =
-    {
-        width: academyPanel.width / 2.1,
-        height: academyPanel.height / 16
-    };
-    academySection.left = academyPanel.width - academySection.width;
+            let seconds = 60 * GameDB.academy.farms[planet * 3 + farm].baseTime / power;
+            seconds = Math.max(seconds, 2);
 
-    setFarmCSS(lineHeight);
+            console.log(seconds);
 
-    const panelElem = document.createElement('div');
-    document.body.append(panelElem);
-    panelElem.classList.add('panel');
-    panelElem.style.width = `${academyPanel.width}px`;
-    panelElem.style.height = `${academyPanel.height}px`;
-    academyPanel.elem = panelElem;
+            if (getRawTime) { farmData.push({time: seconds, personnel: population}); continue; }
 
-    academyPanel.zeusElem = [];
+            let hours = Math.floor(seconds / 3600);
+            seconds -= hours * 3600;
 
-    const zeusHeader = document.createElement('label');
-    academyPanel.elem.appendChild(zeusHeader);
-    zeusHeader.innerText = 'Zeus';
-    zeusHeader.classList.add('sectionHeader');
-    zeusHeader.style.left = `10px`;
-    zeusHeader.style.top = `${Math.round(lineHeight / 2 - (zeusHeader.offsetHeight / 2))}px`;
-    academyPanel.zeusElem.push(zeusHeader);
+            let minutes = Math.floor(seconds / 60);
+            seconds -= minutes * 60;
 
-    const rankLabel = document.createElement('label');
-    academyPanel.elem.appendChild(rankLabel);
-    rankLabel.innerText = 'Rank';
-    rankLabel.classList.add('inputLabel');
-    rankLabel.style.left = `10px`;
-    rankLabel.style.top = `${Math.round(lineHeight * 3 / 2 - (rankLabel.offsetHeight / 2))}px`;
-    academyPanel.zeusElem.push(rankLabel);
+            if (hours > 0) { seconds = Math.round(seconds); }
 
-    const rankInp = document.createElement('input');
-    academyPanel.elem.appendChild(rankInp);
-    rankInp.type = 'number';
-    rankInp.value = playerData.fleet.zeus.rank.current;
-    rankInp.classList.add('dataField');
-    rankInp.style.left = `${Math.round(10 + rankLabel.offsetWidth + (rankInp.offsetWidth / 2))}px`;
-    rankInp.style.top = `${Math.round(lineHeight * 3 / 2 - (rankInp.offsetHeight / 2))}px`;
-    rankInp.addEventListener('change', modifyFarmValue);
-    academyPanel.zeusElem.push(rankInp);
-    
-    panelResizeFunction = resizeAcademyPanel;
-    destroyCurrentPanel = destroyAcademyPanel;
-    academyPanel.active = 'farms';
+            let time = Math.round(seconds * 100) / 100;
+            if (minutes > 0 || hours > 0)
+            {
+                time = minutes + ':' + (seconds < 10 ? '0' : '') + time;
+                if (hours > 0)
+                {
+                    time = hours + ':' + (minutes < 10 ? '0' : '') + time;
+                }
+            }
+
+            farmData.push({time, personnel: population});
+        }
+    }
+
+    return farmData;
 }
 
-function constructProjectPanel()
+function GetMaxMissionRate()
 {
+    let farms = [...GameDB.academy.farms];
+    farms.sort((a,b) => {return a.baseTime - b.baseTime;} );
 
-}
+    let speedBoost = (playerData.academy.badges.engineering + 1) * Math.pow(1.05, Math.floor(playerData.research.missionAnalysis3 + 1) / 2) * Math.pow(1.03, playerData.loopMods.swarm);
 
-function constructGearPanel()
-{
-
-}
-
-function constructBadgePanel()
-{
-
-}
-
-function resizeFarmPanel()
-{
-
-}
-
-function resizeProjectPanel()
-{
-
-}
-
-function resizeGearPanel()
-{
-
-}
-
-function resizeBadgePanel()
-{
-
-}
-
-function setFarmCSS(lineHeight)
-{
-    if (academyPanel.panelCSS) document.body.removeChild(academyPanel.panelCSS);
-
-    const newCSS = document.createElement('style');
-    document.body.append(newCSS);
-    const newStyleSheet = newCSS.sheet;
-
-    let selector = '.sectionHeader';
-    let properties =
+    let personnel = 
     [
-        'position: absolute;\n',
-        `font-size: ${Math.round(lineHeight * 0.8)}px;\n`,
-        `line-height: ${Math.round(lineHeight * 0.8)}px;\n`,
-        'color: #FB7CC7;\n',
-        `text-shadow: 0px 0px ${Math.round(lineHeight * 0.8 / 9)}px #FB7CC7;\n`,
-        'margin: auto;\n'
-    ].join('');
+        {
+            power: playerData.academy.personnel[3].power,
+            totalPop: playerData.academy.personnel[3].population,
+            usedPop: 0,
+            get availPop() { return this.totalPop - this.usedPop; }
+        },
+        {
+            power: playerData.academy.personnel[2].power,
+            totalPop: playerData.academy.personnel[2].population,
+            usedPop: 0,
+            get availPop() { return this.totalPop - this.usedPop; }
+        },
+        {
+            power: playerData.academy.personnel[1].power,
+            totalPop: playerData.academy.personnel[1].population,
+            usedPop: 0,
+            get availPop() { return this.totalPop - this.usedPop; }
+        },
+        {
+            power: playerData.academy.personnel[0].power,
+            totalPop: playerData.academy.personnel[0].population,
+            usedPop: 0,
+            get availPop() { return this.totalPop - this.usedPop; }
+        }
+    ];
 
-    newStyleSheet.insertRule(`${selector} { ${properties} }`);
+    farms.forEach(farm =>
+        {
+            if (farm.id === 32)
+            {
+                playerData.academy.farms[2][1].pods = 0;
+                playerData.academy.farms[2][1].fireteams = 0;
+                playerData.academy.farms[2][1].titans = 0;
+                playerData.academy.farms[2][1].corvettes = 0;
+            }
+            else
+            {
+                let planet = Math.floor(farm.id / 10);
+                let farmNum = farm.id - (planet * 10);
 
-    selector = '.inputLabel';
-    properties =
-    [
-        'position: absolute;\n',
-        `font-size: ${Math.round(lineHeight * 0.7)}px;\n`,
-        `line-height: ${Math.round(lineHeight * 0.7)}px;\n`,
-        'color: #FB7CC7;\n',
-        `text-shadow: 0px 0px ${Math.round(lineHeight * 0.8 / 9)}px #FB7CC7;\n`,
-        'margin: auto;\n'
-    ].join('');
+                playerData.academy.farms[planet - 1][farmNum - 1].pods = 0;
+                playerData.academy.farms[planet - 1][farmNum - 1].fireteams = 0;
+                playerData.academy.farms[planet - 1][farmNum - 1].titans = 0;
+                playerData.academy.farms[planet - 1][farmNum - 1].corvettes = 0;
 
-    newStyleSheet.insertRule(`${selector} { ${properties} }`);
+                let farmSpecs =
+                {
+                    maxPop: farm.maxPop,
+                    currentPop: 0,
+                    popDistro: [0, 0, 0, 0],
+                    power: 0,
+                    baseTime: farm.baseTime / speedBoost,
+                    get availSpace() { return this.maxPop - this.currentPop; },
+                    get timeLimitPassed() { return !((this.power === 0) || (this.baseTime * 60 / this.power >= 2)); }
+                }
 
-    selector = '.dataField';
-    properties =
-    [
-        'position: absolute;\n',
-        `height: ${Math.round(lineHeight * 0.9)}px;\n`,
-        `width: ${Math.round(lineHeight * 2.7)}px;\n`,
-        `font-size: ${Math.round(lineHeight * 0.8)}px;\n`,
-        `text-align: center;\n`,
-        'font-family: "Syncopate";\n'
-    ].join('');
+                for (let personnelNum = 0; personnelNum < 4; personnelNum++)
+                {
+                    let populate = Math.min(farmSpecs.availSpace, personnel[personnelNum].availPop);
+                    farmSpecs.currentPop += populate;
+                    farmSpecs.power += populate * personnel[personnelNum].power;
+                    personnel[personnelNum].usedPop += populate;
+                    console.log(GameDB.academy.personnel[3-personnelNum]);
+                    console.log(playerData.academy.farms[planet - 1][farmNum - 1]);
+                    playerData.academy.farms[planet - 1][farmNum - 1][GameDB.academy.personnel[3-personnelNum]] += populate;
 
-    newStyleSheet.insertRule(`${selector} { ${properties} }`);
+                    while (farmSpecs.timeLimitPassed)
+                    {
+                        farmSpecs.currentPop--;
+                        farmSpecs.power -= personnel[personnelNum].power;
+                        personnel[personnelNum].usedPop--;
+                        playerData.academy.farms[planet - 1][farmNum - 1][GameDB.academy.personnel[3-personnelNum]]--;
+                    }
 
-    academyPanel.panelCSS = newCSS;
-}
+                    if (farmSpecs.availSpace === 0) break;
+                }
+            }
+        });
 
-function modifyFarmValue(e)
-{
-
+    SavePlayerData();
 }
